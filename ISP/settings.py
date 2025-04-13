684d6c774d1eed842c778f470d64b0073a0252c1
smallville <abutimartin778@gmail.com>
2025-04-13 03:41:59 +0300

    c1

--- ISP/settings.py
+++ ISP/settings.py
@@ -148,6 +148,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://isp.coolify.kaigates.com'
 ]
 AUTH_USER_MODEL = 'user_dashboard.User'
 
